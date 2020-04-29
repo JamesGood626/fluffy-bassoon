971f16a71ce983ecb60cf5335235f938398008b6
JamesGood626 <jamesgood626@gmail.com>
2020-04-28 21:49:22 -0400

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <h1>NYC Open Data Arrest History</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
